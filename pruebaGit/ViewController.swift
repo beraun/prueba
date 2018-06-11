07964a2848935e788e5fa3af63056da9c76c80fc
Jorge Jesus Beraun Luque <beraun@gmail.com>
2018-06-11 12:19:50 -0500

    Initial Commit

---
+++ pruebaGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pruebaGit
+//
+//  Created by Jorge Jesus Beraun Luque on 11/06/18.
+//  Copyright © 2018 Jorge Jesus Beraun Luque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
